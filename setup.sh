1bdcea45ee90083923c5b7668d2162d9dac20169
Yago <yagoplay1234@gmail.com>
2019-05-27 15:33:21 +0000

    Remove even more fluff

--- setup.sh
+++ setup.sh
@@ -7,22 +7,17 @@ set +e
 mkdir build 2>/dev/null
 cd build
 set -e
-echo 'Preparing build environment'
+echo 'Preparing build environment...'
 cmake -DCMAKE_INSTALL_PREFIX=/usr .. > setup.log ||  echo 'Something went wrong while preparing the source. Check setup.log for more information.'
 
 echo 'Installing requires root privileges.'
-echo 'Building.'
+echo 'bt-opacity will be installed in /opt/material-decoration-bt'
 sudo make -j4 install >> setup.log && echo 'Done building (1/2)' || echo 'Something went wrong while building. Check setup.log for more information.'
-
-echo 'Installing theme utilities'
-echo 'dest: /opt/material-decoration-bt'
 
 set +e
 cd ..
-echo 'target:' "$(pwd)"
 sudo mkdir /opt 2>/dev/null
 sudo cp -r $(pwd) /opt/
-echo 'Installed target source'
 sudo chmod +x /opt/material-decoration-bt/bt-opacity.sh
 sudo chmod +x /opt/material-decoration-bt/chwm.sh
 sudo ln -s /opt/material-decoration-bt/bt-opacity.sh /usr/bin/bt-opacity
